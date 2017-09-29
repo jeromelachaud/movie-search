9ce7f297ff0a083f9e0258f4267746f0e1d4f088
Jerome Lachaud <jerome.lachaud@gmail.com>
2017-09-29 15:55:57 +0100

    Add app index file

---
+++ src/index.js
@@ -0,0 +1,6 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import MovieSearchApp from './MovieSearchApp';
+import './index.css';
+
+ReactDOM.render(<MovieSearchApp />, document.getElementById('mount'));
